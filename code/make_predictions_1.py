1b2e50d9f0b69e2a7390630eedc14efe46e5b096
Paulo Almeida <paulo.miguel.almeida.rodenas@gmail.com>
2018-11-05 12:54:41 +1300

    Add git ignore Generate 1 file per threshold (incomplete)

--- code/make_predictions_1.py
+++ code/make_predictions_1.py
@@ -2,24 +2,23 @@
 Title           :make_predictions_1.py
 Description     :This script makes predictions using the 1st trained model and generates a submission file.
 Author          :Adil Moujahid
+Contributor     :Paulo Miguel Almeida
 Date Created    :20160623
-Date Modified   :20160625
-version         :0.2
+Date Modified   :20181105
+version         :0.3
 usage           :python make_predictions_1.py
-python_version  :2.7.11
+python_version  :3.6.x
 '''
 
-import os
 import glob
 import cv2
 import caffe
-import lmdb
 import numpy as np
 from caffe.proto import caffe_pb2
 
-caffe.set_mode_gpu() 
+caffe.set_mode_gpu()
 
-#Size of images
+# Size of images
 IMAGE_WIDTH = 227
 IMAGE_HEIGHT = 227
 
@@ -27,15 +26,15 @@ IMAGE_HEIGHT = 227
 Image processing helper function
 '''
 
-def transform_img(img, img_width=IMAGE_WIDTH, img_height=IMAGE_HEIGHT):
 
-    #Histogram Equalization
+def transform_img(img, img_width=IMAGE_WIDTH, img_height=IMAGE_HEIGHT):
+    # Histogram Equalization
     img[:, :, 0] = cv2.equalizeHist(img[:, :, 0])
     img[:, :, 1] = cv2.equalizeHist(img[:, :, 1])
     img[:, :, 2] = cv2.equalizeHist(img[:, :, 2])
 
-    #Image Resizing
-    img = cv2.resize(img, (img_width, img_height), interpolation = cv2.INTER_CUBIC)
+    # Image Resizing
+    img = cv2.resize(img, (img_width, img_height), interpolation=cv2.INTER_CUBIC)
 
     return img
 
@@ -43,7 +42,7 @@ def transform_img(img, img_width=IMAGE_WIDTH, img_height=IMAGE_HEIGHT):
 '''
 Reading mean image, caffe model and its weights 
 '''
-#Read mean image
+# Read mean image
 mean_blob = caffe_pb2.BlobProto()
 with open('/home/ubuntu/workspace/deeplearning-cats-dogs-tutorial/input/mean.binaryproto', 'rb') as f:
     data = f.read()
@@ -51,46 +50,56 @@ with open('/home/ubuntu/workspace/deeplearning-cats-dogs-tutorial/input/mean.bin
 mean_array = np.asarray(mean_blob.data, dtype=np.float32).reshape(
     (mean_blob.channels, mean_blob.height, mean_blob.width))
 
+# Read model architecture and trained model's weights
+net = caffe.Net(
+    '/home/ubuntu/workspace/deeplearning-cats-dogs-tutorial/caffe_models/caffe_model_1/caffenet_deploy_1.prototxt',
+    '/home/ubuntu/workspace/deeplearning-cats-dogs-tutorial/caffe_models/caffe_model_1/caffe_model_1_iter_30000.caffemodel',
+    caffe.TEST)
 
-#Read model architecture and trained model's weights
-net = caffe.Net('/home/ubuntu/workspace/deeplearning-cats-dogs-tutorial/caffe_models/caffe_model_1/caffenet_deploy_1.prototxt',
-                '/home/ubuntu/workspace/deeplearning-cats-dogs-tutorial/caffe_models/caffe_model_1/caffe_model_1_iter_30000.caffemodel',
-                caffe.TEST)
-
-#Define image transformers
+# Define image transformers
 transformer = caffe.io.Transformer({'data': net.blobs['data'].data.shape})
 transformer.set_mean('data', mean_array)
-transformer.set_transpose('data', (2,0,1))
+transformer.set_transpose('data', (2, 0, 1))
 
 '''
 Making predicitions
 '''
-#Reading image paths
+# Reading image paths
 test_img_paths = [img_path for img_path in glob.glob("../input/test1/*jpg")]
 
-#Making predictions
+# Making predictions
 test_ids = []
 preds = []
-for img_path in test_img_paths:
-    img = cv2.imread(img_path, cv2.IMREAD_COLOR)
-    img = transform_img(img, img_width=IMAGE_WIDTH, img_height=IMAGE_HEIGHT)
-    
-    net.blobs['data'].data[...] = transformer.preprocess('data', img)
-    out = net.forward()
-    pred_probas = out['prob']
 
-    test_ids = test_ids + [img_path.split('/')[-1][:-4]]
-    preds = preds + [pred_probas.argmax()]
+for threshold in np.arange(0.1, 1.1, 0.1):
+    path = "../caffe_models/caffe_model_1/model_evaluation_tr_%s_.csv".format(str(threshold).replace('.', '_'))
 
-    print(img_path)
-    print(pred_probas.argmax())
-    print('-------')
+    with open(path, "w") as f:
+        f.write("ID,LABEL,THRESHOLD,TP,TN,FP,FN\n")
 
-'''
-Making submission file
-'''
-with open("../caffe_models/caffe_model_1/submission_model_1.csv","w") as f:
-    f.write("id,label\n")
-    for i in range(len(test_ids)):
-        f.write(str(test_ids[i])+","+str(preds[i])+"\n")
-f.close()
+        for img_path in test_img_paths:
+            img = cv2.imread(img_path, cv2.IMREAD_COLOR)
+            img = transform_img(img, img_width=IMAGE_WIDTH, img_height=IMAGE_HEIGHT)
+
+            net.blobs['data'].data[...] = transformer.preprocess('data', img)
+            out = net.forward()
+            pred_probas = out['prob']
+
+            img_name = img_path.split('/')[-1][:-4]
+            preds = preds + [pred_probas.argmax()]
+
+            print(img_path)
+            print(pred_probas)
+            print(pred_probas.argmax())
+            print('-------')
+
+            f.write("%s,%s,%s,%s,%s,%s,%s\n".format(
+                img_name,
+                str(pred_probas.argmax()),
+                str(threshold),
+                "0",
+                "0",
+                "0",
+                "0"
+            ))
+    f.close()
