Fix f-measure calculation

code/make_predictions_1.py
@@ -136,6 +136,6 @@ for threshold in np.arange(0.1, 1.1, 0.1):
         f.write(",,,,,,\n")
         f.write("Precision,{:.8f},,,,,\n".format((count_tp/(count_tp+count_fp))))
         f.write("Recall,{:.8f},,,,,\n".format((count_tp/(count_tp+count_fn))))
-        f.write("F-Measure,{:.8f},,,,,\n".format(1/(0.9*(1/((count_tp/(count_tp+count_fp))/100))+(1-0.9)*(1/((count_tp/(count_tp+count_fn))/100)))))
+        f.write("F-Measure,{:.8f},,,,,\n".format(1/(0.9*(1/(count_tp/(count_tp+count_fp)))+(1-0.9)*(1/(count_tp/(count_tp+count_fn))))))
 
     f.close()